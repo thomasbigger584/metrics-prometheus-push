0f7685323509edc86a221b298f544f490173d1d6
Thomas Bigger <thomasbigger584@gmail.com>
2023-03-27 10:02:25 +0100

    adding data on a schedule

--- app/src/main/java/com/twb/app/service/SnmpMetricService.java
+++ app/src/main/java/com/twb/app/service/SnmpMetricService.java
@@ -5,6 +5,7 @@ import com.twb.app.dto.SnmpWalkDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -13,10 +14,22 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.security.SecureRandom;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class SnmpMetricService {
     private static final Logger log = LoggerFactory.getLogger(SnmpMetricService.class);
+    private static final int SCHEDULE_IN_SECONDS = 2;
+    private static final Random RANDOM = new SecureRandom();
+
+    @Scheduled(fixedDelay = SCHEDULE_IN_SECONDS, timeUnit = TimeUnit.SECONDS)
+    public void scheduledSend() {
+        SnmpWalkDTO walk = new SnmpWalkDTO();
+        walk.setIfInOctets(getRandomNumber(10, 100));
+        sendSnmpWalk(walk);
+    }
 
     public SnmpWalkDTO sendSnmpWalk(SnmpWalkDTO walk) {
         if (walk.getSysUpTime() == null) {
@@ -63,5 +76,9 @@ public class SnmpMetricService {
         metric.setName(metricName);
         metric.setValue(123L);
         return metric;
+    }
+
+    private long getRandomNumber(long min, long max) {
+        return RANDOM.nextLong(max - min + 1) + min;
     }
 }
